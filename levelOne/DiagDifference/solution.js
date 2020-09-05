fcd8d8c7dceaeb6d73e7cc93bb29fbda6baafc81
JerryMcDonald <jandh62808@gmail.com>
2020-09-04 21:48:45 -0500

    squareMatrix solution

---
+++ levelOne/DiagDifference/solution.js
@@ -0,0 +1,69 @@
+use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    // Write your code here
+console.log(arr);
+let count = 0;
+
+let lToR = arr.reduce((total, nextArr) => {
+    total = total + nextArr[count][count];
+    count++;
+    return total;
+}, 0)
+
+
+let rToL = arr.reduce((total, nextArr) => {
+    total = total + nextArr[count];
+    count--;
+    return
+}, 0)
+
+console.log(lToR, rToL);
+
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
