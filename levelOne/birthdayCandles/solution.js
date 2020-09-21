f697c80c6440821ed1cf9ac0fd7653238e90f4ee
JerryMcDonald <jandh62808@gmail.com>
2020-09-20 19:14:56 -0500

    BC solution

---
+++ levelOne/birthdayCandles/solution.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let answer = 0;
+    let tallest = 0;
+    candles.forEach(candle => {
+        if (candle > tallest) {
+            tallest = candle;
+            answer = 1;
+        } else if (candle === tallest) {
+            answer += 1;
+        }
+    });
+
+    console.log(answer);
+    console.log(tallest);
+
+    return answer;
+    
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const candlesCount = parseInt(readLine().trim(), 10);
+
+    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+    const result = birthdayCakeCandles(candles);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
