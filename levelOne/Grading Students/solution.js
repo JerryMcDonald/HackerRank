81bf748339ee8383481a78b1470495c7c49abf67
JerryMcDonald <jandh62808@gmail.com>
2020-09-30 17:48:10 -0500

    Grades solution

---
+++ levelOne/Grading Students/solution.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    // Write your code here
+    console.log(grades);
+    let solution = grades.reduce((solutionArray, grade) => {
+
+        let num = 0;
+        let newGrade = grade;
+        while (newGrade % 5 != 0) {
+            num++;
+            newGrade++;
+        }
+        if (num < 3 && newGrade >= 40) {
+            solutionArray.push(newGrade);
+        } else {
+            solutionArray.push(grade);
+        }
+        return solutionArray;
+    }, []);
+
+    return solution;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
